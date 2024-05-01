8b438a99189894486e134e549a1bc26fd9efe522
Vamshix57 <vamshi.krishna@kalvium.community>
2024-05-01 14:24:53 +0530

    Done

---
+++ pro-nutrition/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import Food from './components/Food'
+import Search from './components/Search'
+import FoodData from './resources/FoodData'
+import foodData from './resources/FoodData'
+import React, { useState } from 'react'
+
+
+
+function App(){
+
+  const [search, setSearch] = useState('')
+  const Data = foodData.filter((e)=>{
+    return e.name.toLowerCase().includes(search.toLowerCase())
+  })
+
+  return (
+    <>
+    <h1>PRO NUTRITION🍇</h1>
+    <Search setSearch={setSearch}/>
+    {Data.map((e,i)=>{
+      return <Food key={i} data={e} index={i} />
+    })}
+    {
+      !(Data.length) && <h2>No Food Result Found</h2>
+    }
+    </>
+  )
+}
+
+export default App
